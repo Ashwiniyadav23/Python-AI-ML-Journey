483d2fa3845f4804ca165aeb46f8c1a2551d1a04
Developer <dev@screenshare.com>
2026-08-02 19:34:24 +0530

    matrix multiplication

--- Phase2/Matrix.py
+++ Phase2/Matrix.py
@@ -18,16 +18,28 @@ print("Columns:", columns)
 
 # Matrix addition 
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+
+# rows = len(matrix)
+# columns = len(matrix[0])
+
+# sum = 0
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[i])):
+#         sum = sum + matrix[i][j]
+# print(sum)
+
+
+# matrix multiplication
+matrix_multiplication =[
+    [1,2,3,4],
+    [5,6,7,8]
 ]
-
-rows = len(matrix)
-columns = len(matrix[0])
-
-sum = 0
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        sum = sum + matrix[i][j]
+sum = 1
+for i in range(len(matrix_multiplication)):
+    for j in range(len(matrix_multiplication[i])):
+        sum = sum * matrix_multiplication[i][j]
 print(sum)
