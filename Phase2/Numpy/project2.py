daa016c70dad0dfa050506ded997dbacc5147431
Developer <dev@screenshare.com>
2026-08-03 11:42:47 +0530

    Solved a problem using np

---
+++ Phase2/Numpy/project2.py
@@ -0,0 +1,34 @@
+""" Problem 1
+A student qualifies if
+Marks ≥ 75
+Attendance ≥ 85
+No backlog
+Count qualified students.
+
+Problem 2
+Store qualified students' marks.
+Find
+Highest
+Lowest
+Average
+
+Problem 3
+Scholarship Rules
+Gold: Marks ≥ Highest − 5
+Silver: Marks ≥ Average
+Bronze: Remaining qualified students
+Print all three categories separately.
+"""
+import numpy as np
+
+# Marks
+M = np.array([20, 30, 60, 80, 66, 95])
+
+# Attendance
+A = np.array([90, 60, 40, 87, 95, 12])
+
+# Backlog
+# 0 = No Backlog
+# 1 = Has Backlog
+B = np.array([1, 0, 1, 0, 0, 1])
+
